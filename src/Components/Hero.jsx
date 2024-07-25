d5407a36867e633d7762ac9935db4f1f4fdd5931
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-07-25 23:42:05 +0100

    second commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,6 +5,38 @@ import Time from "./Time";
 import { Cats, HeroImage } from "../assets";
 import { visionCard } from "../Data/data";
 
+export const Line = () => {
+  return (
+    <span>
+      <svg
+        width="100%"
+        height="10"
+        viewBox="0 0 623 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5 0.5C2.51472 0.5 0.5 2.51472 0.5 5C0.5 7.48528 2.51472 9.5 5 9.5V0.5ZM618 9.5H622.5V0.5H618V9.5ZM5 9.5H618V0.5H5V9.5Z"
+          fill="url(#paint0_linear_56_56)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_56_56"
+            x1="5"
+            y1="5.5"
+            x2="618"
+            y2="5.5"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#12B76A" />
+            <stop offset="1" stop-color="#425108" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </span>
+  );
+};
+
 const Hero = () => {
   return (
     <section className={`section ${styles.heroSection}`}>
@@ -15,15 +47,46 @@ const Hero = () => {
           <img src={Cats} className={styles.catsImage} />
 
           <div className={styles.catsSecondColumn}>
-            <h1 className={styles.catsHeading}>
-              Stranger Cats Presale Starts in:
-            </h1>
+            <div className={styles.textContent}>
+              <p className="font-bold">
+                Total Raised = <span className="text-[#CE3DD8]">$0</span>
+              </p>
 
-            <div className="flex justify-center">
-              <Time />
+              <p>
+                <span className="text-[#CE3DD8]">0.01 </span>USDT = 1 $STRANGER
+              </p>
+
+              <p>Next Stage Price = 0.0125 USDT</p>
             </div>
 
-            <div></div>
+            <div>
+              {" "}
+              <Line />
+            </div>
+
+            <div className="flex justify-between gap-5 mt-0">
+              <div>
+                <p className="text-[#CE3DD8] font-bold"> 0 $STRANGER</p>
+                <p>Tokens Sold</p>
+              </div>
+
+              <div>
+                <p>850,000 </p>
+                <p>Remaining</p>
+              </div>
+
+              <div>
+                <p>0% Sold</p>
+              </div>
+            </div>
+
+            <div>
+
+            </div>
+
+            <button className={styles.btn}>Buy Token</button>
+
+            <a href="/">How to buy</a>
           </div>
         </div>
 
