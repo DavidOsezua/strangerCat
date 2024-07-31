c126c463c930f3c9e9724ab823e447aa308b2d94
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-07-31 07:51:14 +0100

    added a text component

---
+++ src/Components/TextComponent.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "./TextComponent.module.css";
+
+const TextComponent = () => {
+  return (
+    <section className={`section`}>
+      <div className={`sectionContainer ${styles.textContainer}`}>
+        <li>
+          Stranger Cats, the deriv you never knew you needed. Inspired by
+          Stranger Things.
+        </li>
+
+        <div>
+          <p className={styles.title}>The ticker is $STRANGER</p>
+          <p>
+            Step into the enchanting world of Stranger Cats, where the playful
+            allure of meme coins meets the electrifying nostalgia of Stranger
+            Things. Built on the super-speedy Solana network, **$STRANGER**
+            isn't just a meme coin; it’s a dynamic community of crypto
+            enthusiasts ready for adventure.
+          </p>
+        </div>
+
+        <p>
+          <span className={styles.title}>$STRANGER</span> cultivates joy and
+          creativity, through rewarding our community. We believe in the magic
+          of memes to bring people together and create unforgettable shared
+          experiences. Our exciting features and reward systems, transform the
+          Stranger Things universe and ushers it into an immersive, rewarding
+          crypto journey.
+        </p>
+
+        <div>
+          <p className={styles.title}>The Fun Stuff</p>
+          <div>
+            <li className={styles.title}>Cat-tastic Airdrops:</li>
+            <p>
+              Hold Stranger Cats tokens and unlock the potential for regular
+              airdrops. The more you hold, the more you earn. Stay tuned for
+              *$STRANGER* rewards that will keep you multiplying pawfits!{" "}
+            </p>
+          </div>
+
+          <div>
+            <li className={styles.title}>Interactive Community Events</li>
+            <p>
+              Dive into our themed events and contests. From Stranger Things
+              trivia quizzes to make your own meme competitions, there’s always
+              a thrilling opportunity to WIN BIG and have FUN!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TextComponent;
